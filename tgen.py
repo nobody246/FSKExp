456ccd94d20ae7454fbc29d52df5ce5b8ca0211a
nobody246 <a.se8seven@gmail.com>
2019-11-24 17:33:55 -0500

    tgen.py takes cli arg for tx string now

--- tgen.py
+++ tgen.py
@@ -4,6 +4,7 @@ from time import sleep
 from FSKChars import englishToFSK, nato
 import signal
 import validate
+import sys
 #todo add as params
 volume = 1
 fs = 44100 
@@ -11,8 +12,10 @@ duration = .12
 lo = 2300.0
 hi = 2600.0
 freqs = [lo,hi]
-useNato = False #set to True to broadcast chars phoenetically
-messageStr =""" 0123456789 2019 2020 1987 1808 KING CARLITUS WAS THE KING OF THE LAND THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG THIS IS A TEST OF THE PUBLIC ANNOUNCEMENT SYSTEM THIS IS NOT AN EMERGENCY IT IS ONLY A TEST 18004444444 TANGOHOTELECFOQUEBECUNIFORMINDIACHARLIEKILOBRAVOROMEOOSCARWHISKEYNOVEMIERFOXTROTOSCARXRAYJULIETTUNIFORMMIKEPAPAECHODELTAOSCARVICTORECHOROMEOTANGOHOTELECHOLIMAALPHAZULUYANKEEDELTAOSCARGOLF ABCDEFGHIJKLMNOPQRSTUVWXYZ      """
+useNato = (len(sys.argv) >= 3) #set to True to broadcast chars phoenetically
+messageStr ="{} ".format(sys.argv[1])
+if not all(x.isalpha() or x.isspace() for x in messageStr):
+      print("Error: Invalid characters detected, only numbers, letters, and spaces allowed.")
 message=""
 m=""
 for s in messageStr.upper():
@@ -54,9 +57,8 @@ def sigHandler(s, fr):
    p.terminate()     
    exit(0)
 signal.signal(signal.SIGINT, sigHandler)
-while True:
-    for m in message:
-       playFreq(freqs[int(m)])
+for m in message:
+      playFreq(freqs[int(m)])
 stream.stop_stream()
 stream.close()
 p.terminate()
